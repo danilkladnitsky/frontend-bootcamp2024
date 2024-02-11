1a5afdf6d64d41afd0cffe71dfbcae9d52b0e04c
Danil Kladnitsky <danyakladnitsky@gmail.com>
2024-02-11 18:23:01 +0300

    todo-app: add mantine provider

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,5 +1,10 @@
+import { MantineProvider, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+const theme = createTheme({});
+
 function App() {
-  return <div></div>;
+  return <MantineProvider theme={theme} defaultColorScheme="dark"></MantineProvider>;
 }
 
 export default App;
